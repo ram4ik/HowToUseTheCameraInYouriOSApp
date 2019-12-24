6730e10e63e876a5fc8e3d63837cbb24f127f96c
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-12-24 13:57:34 +0200

    Initial Commit

---
+++ HowToUseTheCameraInYouriOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HowToUseTheCameraInYouriOSApp
+//
+//  Created by Ramill Ibragimov on 24.12.2019.
+//  Copyright © 2019 Ramill Ibragimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
